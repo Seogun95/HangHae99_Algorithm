8689e905f0909462334d24f72b6fc81e55e0f4ce
Seogun95 <ksj083895@gmail.com>
2023-02-01 16:09:01 +0900

    fix: 문자열을 정수로 바꾸기  JS 풀이 추가

--- 프로그래머스/lv.1/문자열을_정수로_바꾸기/index.js
+++ 프로그래머스/lv.1/문자열을_정수로_바꾸기/index.js
@@ -1,4 +1,6 @@
-function solution(s) {
-    return s * 1;
-}
-solution('1234');
+let solution = (s) => {
+    return ~~s;
+};
+// let solution = (s) => { return s * 1 };
+// let solution = (s) => { return parseInt(s)};
+// let solution = (s) => { return Number(s)};
