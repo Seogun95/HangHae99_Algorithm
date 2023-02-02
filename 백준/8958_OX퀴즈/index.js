23790894869b09b12a419fbc3c0e871bea648a4b
Seogun95 <ksj083895@gmail.com>
2023-02-03 02:49:56 +0900

    feat: 향해99 모의고사 JS 풀이 추가

--- 백준/8958_OX퀴즈/index.js
+++ 백준/8958_OX퀴즈/index.js
@@ -46,3 +46,18 @@ for (let i = 1; i <= num; i++) {
 55
 30
 */
+
+function solution1(str) {
+    let answer = 0;
+    let count = 0;
+    //1. for문을 사용하여 str의 길이만큼 돌려준다.
+    for (let i = 0; i < str.length; i++) {
+        //2. str의 i번째를 순회하며, 값이 'O'와 같다면 count를 증가하고, 아니라면 count를 0으로 초기화
+        str[i] === 'O' ? count++ : (count = 0);
+        //3. for문이 돌때마다 answer에 count된 값 만큼 하나 하나 더해준다.
+        answer += count;
+    }
+    return answer;
+}
+let str = 'OXOOOXXXOXOOXOOOOOXO';
+console.log(solution1(str)); // 27
